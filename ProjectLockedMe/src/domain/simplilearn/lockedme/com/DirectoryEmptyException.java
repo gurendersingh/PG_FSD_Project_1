cf3ac89dcda778fee829f2c70e405ffaa2622bac
Gurender Singh <gurendersingh@gmail.com>
2022-05-08 13:52:40 +0530

    Added the javadocs parameters.

--- ProjectLockedMe/src/domain/simplilearn/lockedme/com/DirectoryEmptyException.java
+++ ProjectLockedMe/src/domain/simplilearn/lockedme/com/DirectoryEmptyException.java
@@ -2,6 +2,10 @@ package domain.simplilearn.lockedme.com;
 
 public class DirectoryEmptyException extends RuntimeException {
 
+	/**
+	 * 
+	 * @param msg - To add the user defined exception when Directory is empty.
+	 */
 	public DirectoryEmptyException(String msg) {
 		super(msg);
 	}
